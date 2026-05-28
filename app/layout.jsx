419981025a90cedb96a704a9c3cb12c290c53258
charmacist117 <charmacist117@gmail.com>
2026-05-28 10:22:54 +0900

    Add files via upload

---
+++ app/layout.jsx
@@ -0,0 +1,14 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "PharmaDev PMS",
+  description: "제품개발 프로젝트 관리 시스템"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  );
+}
